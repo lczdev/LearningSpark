a272a75f79a9e1c81b5766a505ac2f2ecc3075f3
lichuanzhi <444515509@qq.com>
2018-12-03 09:54:27 +0800

    build file

---
+++ src/main/scala/demo/FunctionDemo.scala
@@ -0,0 +1,14 @@
+package demo
+
+/**
+  * @author lichuanzhi   
+  * @date 2018/12/02 
+  */
+object FunctionDemo {
+  def main(args: Array[String]): Unit = {
+    val someNums = Array(1, 2, 3)
+
+  }
+
+
+}
